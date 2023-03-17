d0aff0c8c56b345fe8eb6c931ad208561d358b72
shahwan <shahwan@students.uni-marburg.de>
2023-03-17 17:26:56 +0100

    updates

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -14,8 +14,7 @@ const data = [
     id: 1,
     title: 'E-SCooTER REPARATUR',
     image: reparatur,
-    content:
-      '',
+    content: '',
     buttonText: 'Reparaturtermin Online Buchen',
     href: '#',
   },
@@ -23,8 +22,7 @@ const data = [
     id: 2,
     title: 'VERKAUF',
     image: verkauf,
-    content:
-      '',
+    content: '',
     buttonText: 'E-Scooter Kaufen',
     href: 'https://store.be-scooter.de/',
   },
@@ -64,70 +62,106 @@ const data = [
     buttonText: 'Tour Buchen',
     href: 'https://store.be-scooter.de/collections/touren-1',
   },
-];
+]
 
 // Helper function to check if the display is mobile or not
 const isMobileDisplay = () => {
-  if (typeof window === 'undefined') return false;
-  return window.innerWidth <= 766;
-};
+  if (typeof window === 'undefined') return false
+  return window.innerWidth <= 766
+}
 
 export function Hero() {
-  const [expandedCard, setExpandedCard] = useState(null);
-  const [upperCardsClass, setUpperCardsClass] = useState('');
-  const [LowerCardsClass, setLowerCardsClass] = useState('');
-  const [isMobile, setIsMobile] = useState(false);
-  const [showContent, setShowContent] = useState(false);
+  const [expandedCard, setExpandedCard] = useState(null)
+  const [upperCardsClass1, setUpperCardsClass1] = useState('')
+  const [upperCardsClass2, setUpperCardsClass2] = useState('')
+  const [upperCardsClass3, setUpperCardsClass3] = useState('')
+  const [LowerCardsClass4, setLowerCardsClass4] = useState('')
+  const [LowerCardsClass5, setLowerCardsClass5] = useState('')
+  const [LowerCardsClass6, setLowerCardsClass6] = useState('')
+  const [isMobile, setIsMobile] = useState(false)
+  const [showContent, setShowContent] = useState(false)
 
   // Set initial isMobile state and handle window resize on the client-side
   useEffect(() => {
-    const isMobile = isMobileDisplay();
-    setIsMobile(isMobile);
-    setShowContent(isMobile);
+    const isMobile = isMobileDisplay()
+    setIsMobile(isMobile)
+    setShowContent(isMobile)
 
     const handleResize = () => {
-      const isMobile = isMobileDisplay();
-      setIsMobile(isMobile);
-      setShowContent(isMobile);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+      const isMobile = isMobileDisplay()
+      setIsMobile(isMobile)
+      setShowContent(isMobile)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   const handleMouseEnter = (cardId) => {
-    if (isMobile) return;
+    if (isMobile) return
 
-    setExpandedCard(cardId);
-    if (cardId > 3) {
-      setUpperCardsClass('move-up');
-      setLowerCardsClass('move-down');
+    setExpandedCard(cardId)
+
+    if(cardId == 1){
+      setLowerCardsClass5('bring-bottom-up')
+      setLowerCardsClass6('bring-bottom-up')
+    } else
+    if(cardId == 2){
+      setLowerCardsClass4('bring-bottom-up')
+      setLowerCardsClass6('bring-bottom-up')
+    } else
+    if(cardId == 3){
+      setLowerCardsClass4('bring-bottom-up')
+      setLowerCardsClass5('bring-bottom-up')
+    } else
+    if (cardId == 4) {
+      setUpperCardsClass1('move-up')
+      setLowerCardsClass4('move-down')
+    } else
+    if (cardId == 5) {
+      setUpperCardsClass2('move-up')
+      setLowerCardsClass5('move-down')
+    } else 
+    if (cardId == 6) {
+      setUpperCardsClass3('move-up')
+      setLowerCardsClass6('move-down')
     } else {
-      setUpperCardsClass('');
-      setLowerCardsClass('');
+      setUpperCardsClass1('')
+      setUpperCardsClass2('')
+      setUpperCardsClass3('')
+      setLowerCardsClass4('')
+      setLowerCardsClass5('')
+      setLowerCardsClass6('')
     }
-    setShowContent(true);
-  };
+    setShowContent(true)
+  }
 
   const handleMouseLeave = () => {
-    if (isMobile) return;
+    if (isMobile) return
 
-    setExpandedCard(null);
-    setUpperCardsClass('');
-    setLowerCardsClass('');
-    setShowContent(false);
-  };
+    setExpandedCard(null)
+    setUpperCardsClass1('')
+    setUpperCardsClass2('')
+    setUpperCardsClass3('')
+    setLowerCardsClass4('')
+    setLowerCardsClass5('')
+    setLowerCardsClass6('')
+    setShowContent(false)
+  }
 
   return (
     <div className="mx-auto bg-[#7e92a575] p-4">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         {data.map((card, index) => (
           <div
             key={card.id}
-            className={`relative lg:h-[392px] h-[720px] md:h-[220px] w-full overflow-hidden rounded-3xl transition-all duration-700 ease-in-out ${
-              expandedCard === card.id ? 'lg:h-[50rem] md:h-[29rem]' : ''
-            }  ${index < 3 ? upperCardsClass : ''} ${
-              index >= 3 ? LowerCardsClass : ''
-            }`}
+            className={`relative h-[720px] w-full overflow-hidden rounded-3xl transition-all duration-700 ease-in-out md:h-[220px] lg:h-[392px] ${
+              expandedCard === card.id ? 'md:h-[29rem] lg:h-[50rem]' : ''
+            }  ${index == 0 ? upperCardsClass1 : ''}
+            ${index == 1 ? upperCardsClass2 : ''}
+            ${index == 2 ? upperCardsClass3 : ''}
+            ${index == 3 ? LowerCardsClass4 : ''}
+            ${index == 4 ? LowerCardsClass5 : ''}
+            ${index == 5 ? LowerCardsClass6 : ''}`}
             onMouseEnter={() => handleMouseEnter(card.id)}
             onMouseLeave={handleMouseLeave}
           >
@@ -135,7 +169,7 @@ export function Hero() {
               src={card.image}
               alt={card.title}
               className={`h-full w-full object-cover ${
-                expandedCard === card.id ? 'brightness-75' : 'brightness-50'
+                expandedCard === card.id ? 'brightness-75' : 'brightness-75'
               }`}
             />
             <div
@@ -144,111 +178,118 @@ export function Hero() {
               }`}
             >
               <h2
-                className={`mb-4 lg:text-3xl text-2xl font-semibold text-white top-4 absolute`}
+                className={`absolute top-4 mb-4 text-2xl font-semibold text-white lg:text-3xl`}
               >
                 {card.title}
               </h2>
-              
+
               {(showContent || expandedCard === card.id) && (
                 <>
-               
                   <p
-                    className="lg:text-xl text-lg md:text-sm lg:mb-24 md:mb-0  text-white mt-[230px] "
+                    className="mt-[230px] text-lg text-white md:mb-0 md:text-sm  lg:mb-24 lg:text-xl "
                     dangerouslySetInnerHTML={{ __html: card.content }}
                   ></p>
                   {card.id === 2 && (
-                    <p
-                    className="lg:text-xl text-lg md:text-sm  lg:mb-24 md:mt-10 md:mb-8  text-white "
-                    
-                  >
-                    Komme doch einfach bei uns im &quot;SToRE!&quot; in Heuchelheim vorbei und lasse dich von uns persönlich beraten. Wir helfen dir gerne dabei, den perfekten E-Scooter für deine Bedürfnisse zu finden und bieten dir sogar eine kostenlose Probefahrt an. Wenn du bereits weißt, welchen E-Scooter du haben möchtest, kannst du auch direkt in unserem &quot;SToRE ONLINE!&quot; bestellen. Wir freuen uns darauf, dich kennenzulernen und dir dabei zu helfen, mobil und umweltbewusst unterwegs zu sein.</p>
+                    <p className="text-lg text-white md:mt-10  md:mb-8 md:text-sm lg:mb-24  lg:text-xl ">
+                      Komme doch einfach bei uns im &quot;SToRE!&quot; in
+                      Heuchelheim vorbei und lasse dich von uns persönlich
+                      beraten. Wir helfen dir gerne dabei, den perfekten
+                      E-Scooter für deine Bedürfnisse zu finden und bieten dir
+                      sogar eine kostenlose Probefahrt an. Wenn du bereits
+                      weißt, welchen E-Scooter du haben möchtest, kannst du auch
+                      direkt in unserem &quot;SToRE ONLINE!&quot; bestellen. Wir
+                      freuen uns darauf, dich kennenzulernen und dir dabei zu
+                      helfen, mobil und umweltbewusst unterwegs zu sein.
+                    </p>
                   )}
                   {card.id === 1 && (
-                    <p
-                    className="lg:text-xl md:text-sm  lg:mb-24 md:mt-10 lap:mb-8  text-white mt-[0px]"
-                    
-                  >
-                    Unser Ziel ist es, dass ihr stets sicher und sorgenfrei unterwegs seid. Wir kümmern uns um euren E-Scooter und bieten euch einen umfassenden Service, damit ihr euch auf das Fahren konzentrieren könnt. Dazu gehört:</p>
+                    <p className="lap:mb-8 mt-[0px]  text-white md:mt-10 md:text-sm  lg:mb-24 lg:text-xl">
+                      Unser Ziel ist es, dass ihr stets sicher und sorgenfrei
+                      unterwegs seid. Wir kümmern uns um euren E-Scooter und
+                      bieten euch einen umfassenden Service, damit ihr euch auf
+                      das Fahren konzentrieren könnt. Dazu gehört:
+                    </p>
                   )}
                   {card.id === 1 && (
                     <ul
-                                    role="list"
-                                    className="mr-auto text-md md:text-xs lg:text-lg mt-0 space-y-2 whitespace-nowrap font-semibold text-slate-100"
-                                  >
-                                    <li className="mt-24 flex gap-x-3 md:mt-0 ">
-                                      <CheckIcon
-                                        className="h-6 w-5 flex-none text-slate-100"
-                                        aria-hidden="true"
-                                      />
-                                      eine Reifenpanne zu beheben
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                      <CheckIcon
-                                        className="h-6 w-5 flex-none text-slate-100"
-                                        aria-hidden="true"
-                                      />
-                                      einen Schaden zu beheben
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                      <CheckIcon
-                                        className="h-6 w-5 flex-none text-slate-100"
-                                        aria-hidden="true"
-                                      />
-                                      Verschleißteile zu erneuern
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                      <CheckIcon
-                                        className="h-6 w-5 flex-none text-slate-100"
-                                        aria-hidden="true"
-                                      />
-                                      Probleme zu analysieren und eine <br /> Lösung zu finden.
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                      <CheckIcon
-                                        className="h-6 w-5 flex-none text-slate-100"
-                                        aria-hidden="true"
-                                      />
-                                      Original-Ersatzteile zu beschaffen
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                      <CheckIcon
-                                        className="h-6 w-5 flex-none text-slate-100"
-                                        aria-hidden="true"
-                                      />
-                                      eine persönliche Note in Form von <br /> Pulverbeschichtungen
-                                    </li>
-                                  </ul>
+                      role="list"
+                      className="text-md mr-auto mt-0 space-y-2 whitespace-nowrap font-semibold text-slate-100 md:text-xs lg:text-lg"
+                    >
+                      <li className="mt-24 flex gap-x-3 md:mt-0 ">
+                        <CheckIcon
+                          className="h-6 w-5 flex-none text-slate-100"
+                          aria-hidden="true"
+                        />
+                        eine Reifenpanne zu beheben
+                      </li>
+                      <li className="flex gap-x-3">
+                        <CheckIcon
+                          className="h-6 w-5 flex-none text-slate-100"
+                          aria-hidden="true"
+                        />
+                        einen Schaden zu beheben
+                      </li>
+                      <li className="flex gap-x-3">
+                        <CheckIcon
+                          className="h-6 w-5 flex-none text-slate-100"
+                          aria-hidden="true"
+                        />
+                        Verschleißteile zu erneuern
+                      </li>
+                      <li className="flex gap-x-3">
+                        <CheckIcon
+                          className="h-6 w-5 flex-none text-slate-100"
+                          aria-hidden="true"
+                        />
+                        Probleme zu analysieren und eine <br /> Lösung zu
+                        finden.
+                      </li>
+                      <li className="flex gap-x-3">
+                        <CheckIcon
+                          className="h-6 w-5 flex-none text-slate-100"
+                          aria-hidden="true"
+                        />
+                        Original-Ersatzteile zu beschaffen
+                      </li>
+                      <li className="flex gap-x-3">
+                        <CheckIcon
+                          className="h-6 w-5 flex-none text-slate-100"
+                          aria-hidden="true"
+                        />
+                        eine persönliche Note in Form von <br />{' '}
+                        Pulverbeschichtungen
+                      </li>
+                    </ul>
                   )}
-                  <div className='mt-auto mb-10 w-full text-center'>
-                  <a
-                href={card.href}
-                className="mt-24 md:mt-12 block rounded-md border-[#4a6d92] bg-[#4a6d92] py-3 px-4 font-bold text-white hover:bg-[#3f5872] hover:-translate-y-1 transition duration-300"
-              >
-                {card.buttonText}
-              </a>
-              {card.id === 1 && (
+                  <div className="mt-auto mb-10 w-full text-center">
                     <a
-                    href={card.href}
-                    className="lg:mt-44 mt-24 md:mt-32 block rounded-md border-[#4a6d92] bg-[#4a6d92] py-3 px-4 font-bold text-white hover:bg-[#3f5872] hover:-translate-y-1 transition duration-300"
-                  >
-                    {card.buttonText}
-                  </a>
-                  )}
-                  {card.id === 2 && (
-                    <a
-                    href={card.href}
-                    className="lg:mt-44 mt-20 md:mt-32 block rounded-md border-[#4a6d92] bg-[#4a6d92] py-3 px-4 font-bold text-white hover:bg-[#3f5872] hover:-translate-y-1 transition duration-300"
-                  >
-                    {card.buttonText}
-                  </a>
-                  )}
-              </div>
-                  
+                      href={card.href}
+                      className="mt-24 block rounded-md border-[#4a6d92] bg-[#4a6d92] py-3 px-4 font-bold text-white transition duration-300 hover:-translate-y-1 hover:bg-[#3f5872] md:mt-12"
+                    >
+                      {card.buttonText}
+                    </a>
+                    {card.id === 1 && (
+                      <a
+                        href={card.href}
+                        className="mt-24 block rounded-md border-[#4a6d92] bg-[#4a6d92] py-3 px-4 font-bold text-white transition duration-300 hover:-translate-y-1 hover:bg-[#3f5872] md:mt-32 lg:mt-44"
+                      >
+                        {card.buttonText}
+                      </a>
+                    )}
+                    {card.id === 2 && (
+                      <a
+                        href={card.href}
+                        className="mt-20 block rounded-md border-[#4a6d92] bg-[#4a6d92] py-3 px-4 font-bold text-white transition duration-300 hover:-translate-y-1 hover:bg-[#3f5872] md:mt-32 lg:mt-44"
+                      >
+                        {card.buttonText}
+                      </a>
+                    )}
+                  </div>
                 </>
               )}
             </div>
-            {!expandedCard && (
-              <div className="hidden md:block absolute inset-x-0 top-1/2 mt-[-1.5em]">
+            {expandedCard !== card.id && (
+              <div className="absolute inset-x-0 top-1/2 mt-[-1.5em] hidden md:block">
                 <h2 className=" text-center text-3xl font-semibold text-white">
                   {card.title}
                 </h2>
